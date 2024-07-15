579c3ba4827bc54d4892aa564438c1e23d91a5a2
SelinLiu2023 <seline.lxy@gmail.com>
2024-07-15 08:31:46 +0200

    update

--- index.mjs
+++ index.mjs
@@ -1,18 +1,5 @@
 import readline from 'readline';
 import fs from 'fs';
-// import chalk from 'chalk';
-const NO_FIND = -1;
-const COLOR_RESET = '\x1b[0m';
-const COLOR_RED = '\x1b[31m';
-const COLOR_GREEN = '\x1b[32m';
-const COLOR_BLUE = '\x1b[34m';
-const UNDERSCORE = '\x1b[4m';
-const BRIGHT = '\x1b[1m';
-const RED_COLOR = 1;
-const GREEN_COLOR = 2;
-const NO_COLOR = 0;
-const ROW_BEGIN = `${COLOR_BLUE}| ${COLOR_RESET}`;
-const ROW_GAP = `${COLOR_BLUE} | ${COLOR_RESET}`;
 let myTable = {
     columnsSetting : [
         { key: 'id', title: 'Id', width: 5, datatype: "text"},
@@ -28,152 +15,171 @@ let myTable = {
     ],
     urgentDate : 0,
     title : "Happy Birthday",
-    sortByTitle : "id",
+    sortByKey : "id",
     }
-    function printTitleRow(){
-        const tableWidth = myTable.columnsSetting.reduce((accu, curr)=>accu + curr.width, 0);
-        let spaceCount = Math.ceil((tableWidth - myTable.title.length) / 2);
-        spaceCount = spaceCount < 0 ? 0: spaceCount;
-        const outputRow = " ".repeat(spaceCount) + `${COLOR_BLUE}${BRIGHT}${UNDERSCORE}${myTable.title}${COLOR_RESET}`;
-        console.log("");
-        console.log(outputRow);
-        console.log("");
+const NO_FIND = -1;
+const COLOR_RESET = '\x1b[0m';
+const COLOR_RED = '\x1b[31m';
+const COLOR_GREEN = '\x1b[32m';
+const COLOR_BLUE = '\x1b[34m';
+const UNDERSCORE = '\x1b[4m';
+const BRIGHT = '\x1b[1m';
+const RED_COLOR = 1;
+const GREEN_COLOR = 2;
+const NO_COLOR = 0;
+const ORIGINAL_ROW_GAP = " | ";
+const ROW_GAP = `${COLOR_BLUE} | ${COLOR_RESET}`;
+const ROW_BEGIN = `${COLOR_BLUE}| ${COLOR_RESET}`;
+function tableTerminalDistance(){
+    const terminalWidth = process.stdout.columns;
+    const tableWidth = myTable.columnsSetting.reduce((accu,row)=>accu + row.width, 0) + (myTable.columnsSetting.length - 1) * ORIGINAL_ROW_GAP.length + 2 * 2;
+    return " ".repeat(Math.round((terminalWidth - tableWidth) / 2));
+}
+function printTitleRow(){
+    const terminalWidth = process.stdout.columns;
+    let spaceCount = Math.round((terminalWidth - myTable.title.length) / 2);
+    spaceCount = spaceCount < 0 ? 0: spaceCount;
+    const outputRow = " ".repeat(spaceCount) + `${COLOR_BLUE}${BRIGHT}${UNDERSCORE}${myTable.title}${COLOR_RESET}`;
+    console.log("");
+    console.log(outputRow);
+    console.log("");
+}
+function printHeaderRow(){
+    let outputRow = tableTerminalDistance() + ROW_BEGIN;
+    for(let i = 0; i < myTable.columnsSetting.length; i ++){
+        outputRow += `${COLOR_BLUE}${myTable.columnsSetting[i].title.padEnd(myTable.columnsSetting[i].width)}${COLOR_RESET}` + ROW_GAP;
     }
-    function printHeaderRow(){
-        let outputRow = ROW_BEGIN;
-        for(let i = 0; i < myTable.columnsSetting.length; i ++){
-            outputRow += `${COLOR_BLUE}${myTable.columnsSetting[i].title.padEnd(myTable.columnsSetting[i].width)}${COLOR_RESET}` + ROW_GAP;
+    console.log(outputRow);
+    printDividerRow();
+}
+function printDividerRow(){
+    let outputRow = tableTerminalDistance() + ROW_BEGIN;
+    for(let i = 0; i < myTable.columnsSetting.length; i ++){
+        outputRow += "".padEnd(myTable.columnsSetting[i].width,"-") + ROW_GAP;
+    }
+    console.log(`${COLOR_BLUE}${outputRow}${COLOR_RESET}`);
+}
+function wrapString(str,num){
+    let string = str.toString();
+    let arr = [];
+    let index = 0;
+    const arrStrLength = num;
+    while(string.length > arrStrLength){
+        arr[index] = string.slice(0,arrStrLength);
+        const spaceIndex = arr[index].lastIndexOf(" ");
+        if(spaceIndex !== -1){
+            arr[index] = string.slice(0,spaceIndex).padEnd(arrStrLength);
+            string = string.slice(spaceIndex + 1);  
         }
-        console.log(outputRow);
-        printDividerRow();
+        else string = string.slice(arrStrLength);
+        index ++;
     }
-    function printDividerRow(){
-        let outputRow = ROW_BEGIN;
-        for(let i = 0; i < myTable.columnsSetting.length; i ++){
-            outputRow += "".padEnd(myTable.columnsSetting[i].width,"-") + ROW_GAP;
-        }
-        console.log(`${COLOR_BLUE}${outputRow}${COLOR_RESET}`);
+    arr[index] = string.padEnd(arrStrLength);
+    return arr;
+}
+function printRow(row, color){
+    const copyRow = {};
+    Object.assign(copyRow, row);
+    const rowCounts = [];
+    for(let key in copyRow){
+        const propWidth = myTable.columnsSetting.find(a => a.key === key).width;
+        copyRow[key] = wrapString(copyRow[key], propWidth);
+        rowCounts.push(copyRow[key].length);
     }
-    function wrapString(str,num){
-        let string = str.toString();
-        let arr = [];
-        let index = 0;
-        const arrStrLength = num;
-        while(string.length > arrStrLength){
-            arr[index] = string.slice(0,arrStrLength);
-            const spaceIndex = arr[index].lastIndexOf(" ");
-            if(spaceIndex !== -1){
-                arr[index] = string.slice(0,spaceIndex).padEnd(arrStrLength);
-                string = string.slice(spaceIndex + 1);  
+    const maxRowCounts = Math.max(...rowCounts);
+    for(let i = 0; i < maxRowCounts; i ++){
+        let outputRow = tableTerminalDistance() + ROW_BEGIN;
+        for(let item of myTable.columnsSetting){
+            const prop = item.key;
+            if(prop in copyRow){
+                if(color === 1){
+                    outputRow += `${COLOR_RED}${(copyRow[prop][i] ?? " ".padEnd(item.width))}${COLOR_RESET}` + ROW_GAP;
+                }else if(color === 2){
+                    outputRow += `${COLOR_GREEN}${(copyRow[prop][i] ?? " ".padEnd(item.width))}${COLOR_RESET}` + ROW_GAP;
+                }else
+                    outputRow += (copyRow[prop][i] ?? " ".padEnd(item.width)) + ROW_GAP;
+            }else{
+                outputRow += " ".padEnd(item.width) + ROW_GAP;
             }
-            else string = string.slice(arrStrLength);
-            index ++;
         }
-        arr[index] = string.padEnd(arrStrLength);
-        return arr;
+        console.log(outputRow);
     }
-    function printRow(row, color){
-        const copyRow = {};
-        Object.assign(copyRow, row);
-        const rowCounts = [];
-        for(let key in copyRow){
-            const propWidth = myTable.columnsSetting.find(a => a.key === key).width;
-            copyRow[key] = wrapString(copyRow[key], propWidth);
-            rowCounts.push(copyRow[key].length);
-        }
-        const maxRowCounts = Math.max(...rowCounts);
-        for(let i = 0; i < maxRowCounts; i ++){
-            let outputRow = ROW_BEGIN;
-            for(let item of myTable.columnsSetting){
-                const prop = item.key;
-                if(prop in copyRow){
-                    if(color === 1){
-                        outputRow += `${COLOR_RED}${(copyRow[prop][i] ?? " ".padEnd(item.width))}${COLOR_RESET}` + ROW_GAP;
-                    }else if(color === 2){
-                        outputRow += `${COLOR_GREEN}${(copyRow[prop][i] ?? " ".padEnd(item.width))}${COLOR_RESET}` + ROW_GAP;
-                    }else
-                        outputRow += (copyRow[prop][i] ?? " ".padEnd(item.width)) + ROW_GAP;
-                }else{
-                    outputRow += " ".padEnd(item.width) + ROW_GAP;
-                }
-            }
-            console.log(outputRow);
-        }
-        printDividerRow();
-    }
+    printDividerRow();
+}
 
-    function printRows(){
-        console.log("");
-        if(myTable.title !== "") printTitleRow();
-        printHeaderRow();
-        sortRows();
-        for(let i = 0; i < myTable.urgentDate; i ++) 
-            printRow(myTable.rows[i], GREEN_COLOR);
-        for(let i = myTable.urgentDate; i < myTable.rows.length; i++)
-            printRow(myTable.rows[i], NO_COLOR);
-        console.log("");
+function printRows(){
+    console.log("");
+    if(myTable.title !== "") printTitleRow();
+    printHeaderRow();
+    sortRows();
+    for(let i = 0; i < myTable.urgentDate; i ++) 
+        printRow(myTable.rows[i], GREEN_COLOR);
+    for(let i = myTable.urgentDate; i < myTable.rows.length; i++)
+        printRow(myTable.rows[i], NO_COLOR);
+    console.log("");
+}
+function sortRows(){
+    const sortKey = myTable.sortByKey;
+    const column = myTable.columnsSetting.find(a => a.key === sortKey);
+    if(column === undefined || column.datatype === undefined) 
+        return;
+    if(column.datatype === "date") sortRowsByDate(column.key);
+    else if (column.datatype === "text") sortRowsByDefault(column.key);
+}
+function sortRowsByDefault(key){
+    const emptyRows = myTable.rows.filter(a => a[key] === "" || a[key] === undefined);
+    const notEmptyRows = myTable.rows.filter(a=>a[key] !== "" && a[key] !== undefined);
+    myTable.rows = notEmptyRows.sort((a,b)=>a[key].toString().toLowerCase().localeCompare(b[key].toString().toLowerCase()));
+    myTable.rows = notEmptyRows.concat(emptyRows);
+    let id = 1;
+    for(let row of myTable.rows){
+        row.id = id;
+        id ++;
     }
-    function sortRows(){
-        const sortTitle = myTable.sortByTitle;
-        const column = myTable.columnsSetting.find(a => a.title === sortTitle);
-        if(column === undefined || column.datatype === undefined) 
-            return;
-        if(column.datatype === "date") sortRowsByDate(column.key);
-        else if (column.datatype === "text") sortRowsByDefault(column.key);
+}
+function sortRowsByDate(key){
+    const today = new Date();
+    const thisMonth = today.getUTCMonth() + 1;
+    const thisDay = today.getUTCDate();
+    const thisYear = today.getUTCFullYear();
+    const emptyRows = myTable.rows.filter(a => a[key] === "" || a[key] === undefined);
+    const notEmptyRows = myTable.rows.filter(a=>a[key] !== "" && a[key] !== undefined);
+    notEmptyRows.sort((a,b)=>{
+        if(!a[key] || !b[key]) return 0;
+        const [aDay,aMonth] = a[key].split("-").map(Number);            
+        const [bDay,bMonth] = b[key].split("-").map(Number);
+        const aDate = new Date(thisYear, aMonth - 1, aDay);
+        const bDate = new Date(thisYear, bMonth - 1, bDay);
+        if(aDate < today) aDate.setFullYear(thisYear + 1);
+        if(bDate < today) bDate.setFullYear(thisYear + 1);
+        return aDate - bDate;
+    });
+    myTable.rows = notEmptyRows.concat(emptyRows);
+    let id = 1;
+    for(let row of myTable.rows){
+        row.id = id;
+        id ++;
     }
-    function sortRowsByDefault(key){
-        const emptyRows = myTable.rows.filter(a => a[key] === "");
-        myTable.rows = myTable.rows.filter(a=>a[key] !== "").sort((a,b)=>a[key].toLowerCase().localeCompare(b[key].toLowerCase()));
-        myTable.rows = myTable.rows.concat(emptyRows);
-        let id = 1;
-        for(let row of myTable.rows){
-            row.id = id;
-            id ++;
-        }
+    myTable.urgentDate = notEmptyRows.filter(a =>{
+        const twoWeeks = 2* 7 * 24 * 60 * 60 * 1000;
+        const [aDay,aMonth] = a[key].split("-").map(Number); 
+        const aDate = new Date(thisYear, aMonth - 1, aDay);
+        if(aDate < today) aDate.setFullYear(thisYear + 1);
+        if((aDate - today) < twoWeeks) return true;
+    }).length;
+}
+function saveTable(){
+    const saveData = JSON.stringify(myTable);
+    fs.writeFileSync("./myTable.json", saveData, 'utf8');
+}
+function restoreTable(){
+    if (fs.existsSync("./myTable.json")) {
+        const restoreData = fs.readFileSync("./myTable.json", 'utf8');
+        myTable = JSON.parse(restoreData);
+    }else {
+        saveTable();
     }
-    function sortRowsByDate(key){
-        const today = new Date();
-        const thisMonth = today.getUTCMonth() + 1;
-        const thisDay = today.getUTCDate();
-        const thisYear = today.getUTCFullYear();
-        const emptyRows = myTable.rows.filter(a => a[key] === "");
-        let notEmptyRows = myTable.rows.filter(a=>a[key] !== "");
-        notEmptyRows.sort((a,b)=>{
-            if(!a[key] || !b[key]) return 0;
-            const [aDay,aMonth] = a[key].split("-").map(Number);            
-            const [bDay,bMonth] = b[key].split("-").map(Number);
-            const aDate = new Date(thisYear, aMonth - 1, aDay);
-            const bDate = new Date(thisYear, bMonth - 1, bDay);
-            if(aDate < today) aDate.setFullYear(thisYear + 1);
-            if(bDate < today) bDate.setFullYear(thisYear + 1);
-            return aDate - bDate;
-        });
-        myTable.rows = notEmptyRows.concat(emptyRows);
-        let id = 1;
-        for(let row of myTable.rows){
-            row.id = id;
-            id ++;
-        }
-        myTable.urgentDate = myTable.rows.filter(a =>{
-            const twoWeeks = 2* 7 * 24 * 60 * 60 * 1000;
-            const [aDay,aMonth] = a[key].split("-").map(Number); 
-            const aDate = new Date(thisYear, aMonth - 1, aDay);
-            if(aDate < today) aDate.setFullYear(thisYear + 1);
-            if((aDate - today) < twoWeeks) return true;
-        }).length;
-    }
-    function saveTable(){
-        const saveData = JSON.stringify(myTable);
-        fs.writeFileSync("./myTable.json", saveData, 'utf8');
-    }
-    function restoreTable(){
-        if (fs.existsSync("./myTable.json")) {
-            const restoreData = fs.readFileSync("./myTable.json", 'utf8');
-            myTable = JSON.parse(restoreData);
-        }else {
-            saveTable();
-        }
-    }
+}
 function commandList(){
     rl.question("Please enter command: ", (answer) => {
         if (answer.trim().toLowerCase() === 'exit') {
@@ -196,6 +202,8 @@ function commandList(){
             configEditCol();
         } else if (answer.trim().toLowerCase() === 'config remove column') {
             configRemoveCol();
+        } else if (answer.trim().toLowerCase() === 'show table setting') {
+            showTableSetting();
         } else if (answer.trim().toLowerCase() === 'config table title') {
             configTableTitle();
         } else if (answer.trim().toLowerCase() === 'help') {
@@ -352,7 +360,7 @@ function sortReadline(){
                 sortRowsByDefault(inputStr);
                 myTable.urgentDate = 0;
             } 
-            myTable.sortByTitle = column.title;
+            myTable.sortByKey = column.key;
             showMain();
         }
     });
@@ -376,8 +384,25 @@ function configAddCol(){
                     if(colType === "date") newCol.datatype = "date";
                     else if (colType === "text") newCol.datatype = "text";
                     else newCol.datatype = "text";
-                    myTable.columnsSetting.push(newCol);
-                    showMain();
+                    rl.question("Enter the position of this column( a number biger as 0) : ", (input)=>{
+                        const colPosition = Number(input.trim());
+                        if(Number.isInteger(colPosition)){
+                            if(colPosition === 0){
+                            console.log("Position 0 is reserved for Id.");
+                            commandList();
+                            }else if(colPosition > 0 && colPosition < myTable.columnsSetting.length){
+                                myTable.columnsSetting.splice(colPosition - 1, 0, newCol);
+                                showMain();
+                            }
+                            else{
+                                myTable.columnsSetting.push(newCol);
+                                showMain();
+                            }
+                        }else{
+                            console.log("Invalid");
+                            commandList();
+                        }
+                    });
                 });
             }else{
                 console.log("Invalid");
@@ -388,17 +413,24 @@ function configAddCol(){
 }
 function configRemoveCol(){
     rl.question("Enter column title : ", (input)=>{
-        const removeKey = input.trim().toLowerCase();
-        const colIndex = myTable.columnsSetting.findIndex(a=>a.key === removeKey);
+        const removeTitle = input.trim().toLowerCase();
+        const colIndex = myTable.columnsSetting.findIndex(a=>a.title.toLowerCase() === removeTitle);
         if(colIndex === NO_FIND){
             console.log("This column doens't exist in table.");
             commandList(); 
-        }else if(removeKey === "id"){
+        }else if(myTable.columnsSetting[colIndex].key === "id"){
             console.log("This column is not allowed to be removed.");
             commandList(); 
         }
-        rl.question(`Do you really want to remove ${COLOR_RED}${removeKey}${COLOR_RESET} column?\n${COLOR_RED}All content will be lost. y/n ${COLOR_RESET}: `,(answer)=>{
+        rl.question(`Do you really want to remove ${COLOR_RED}${removeTitle}${COLOR_RESET} column?\n${COLOR_RED}All content will be lost. y/n ${COLOR_RESET}: `,(answer)=>{
             if (answer.trim().toLowerCase() === 'y') {
+                const removeKey = myTable.columnsSetting[colIndex].key;
+                if(myTable.sortByKey === removeKey){
+                    myTable.sortByKey = "id";
+                }
+                if(myTable.columnsSetting[colIndex].datatype === "date"){
+                    myTable.urgentDate = 0;
+                }
                 myTable.columnsSetting.splice(colIndex,1);
                 for(let row of myTable.rows){
                     if(removeKey in row)  delete row[removeKey];
@@ -450,7 +482,7 @@ function editColumnEntry(entry, column){
             }else if(colType === "date"){
                 const result = convertColumnTypeDate(column.key);
                 if(result){
-                    column.type = colType;
+                    column.datatype = colType;
                     showMain();
                 }else{
                     console.log("Content doesn't fit in date format. Change column datatype failed. ");
@@ -464,7 +496,7 @@ function editColumnEntry(entry, column){
     }
 }
 function convertColumnTypeDate(key){
-    const contentArr = myTable.rows.map(row => row[key]).filter(row => row !== "");
+    const contentArr = myTable.rows.filter(row => row[key] !== "" &&  row[key] !== undefined).map(row => row[key]);
     const checkArr = contentArr.filter(row => isValidDate(row));
     return contentArr.length === checkArr.length;
 }
@@ -487,14 +519,12 @@ function configTableTitle(){
         showMain();
     });
 }
-// console.log(`        
-//          ,   ,   ,   ,   , 
-//         _|___|___|___|___|___
-//         |   HAPPY BIRTHDAY  |
-//     =============================
-//     |~-~-~-~-~-~-~-~-~-~~-~-~-~-|
-//     |~-~-~-~-~-~-~-~-~-~~-~-~-~-|
-//     =============================\n`);
+function showTableSetting(){
+    console.table(myTable.columnsSetting);
+    console.log(`sort by : ${myTable.sortByKey}`);
+    console.log(`table title : ${myTable.title}`);
+    commandList(); 
+}
 restoreTable();
 printRows();
 const menuInfo = `
@@ -512,6 +542,7 @@ You can also enter command to modify the configration of table.
 ${COLOR_BLUE}${UNDERSCORE}${BRIGHT}config add column${COLOR_RESET} :      add a column to table.
 ${COLOR_BLUE}${UNDERSCORE}${BRIGHT}config remove column${COLOR_RESET} :   remove a column from table.
 ${COLOR_BLUE}${UNDERSCORE}${BRIGHT}config edit column${COLOR_RESET} :    modify a column.
+${COLOR_BLUE}${UNDERSCORE}${BRIGHT}show table setting${COLOR_RESET} :    show setting of this table
 ${COLOR_BLUE}${UNDERSCORE}${BRIGHT}config table title${COLOR_RESET} :    modify table title.\n`;
 console.log(menuInfo);
 const rl = readline.createInterface({
